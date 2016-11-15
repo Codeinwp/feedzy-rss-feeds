d7db52cc50f838eb030fe6a50f640ee4982886ae
Preda Bogdan Win <bogdan@opendevelopers.ro>
2016-11-15 17:43:47 +0200

    Small style changes

--- admin/class-abstract-feedzy-rss-feeds-admin.php
+++ admin/class-abstract-feedzy-rss-feeds-admin.php
@@ -41,7 +41,7 @@ abstract class Feedzy_Rss_Feeds_Abstract {
 	 * @return  string
 	 */
 	public function feedzy_default_error_notice( $error, $feedURL  ) {
-		error_log( 'Feedzy RSS Feeds - related feed: ' . $feedURL . ' - Error message: ' . $this->feedzy_array_obj_string( $error ) );
+		error_log( 'Feedzy RSS Feeds - related feed: ' . print_r( $feedURL ) . ' - Error message: ' . $this->feedzy_array_obj_string( $error ) );
 		return '<div id="message" class="error" data-error"' . esc_attr( $this->feedzy_array_obj_string( $error ) ) . '"><p>' . __( 'Sorry, this feed is currently unavailable or does not exists anymore.', 'feedzy-rss-feeds-translate' ) . '</p></div>';
 	}
 
