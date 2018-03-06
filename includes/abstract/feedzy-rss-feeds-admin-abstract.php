22812c2c0339621f9e2a87be0691e9f763878bb1
contactashish13 <contactashish13@gmail.com>
2018-03-06 10:51:39 +0530

    add missing function

--- includes/abstract/feedzy-rss-feeds-admin-abstract.php
+++ includes/abstract/feedzy-rss-feeds-admin-abstract.php
@@ -331,6 +331,30 @@ abstract class Feedzy_Rss_Feeds_Admin_Abstract {
 	}
 
 	/**
+	 * Convert back from parse_url.
+	 *
+	 * @param $parsed_url
+	 *
+	 * @return string
+	 */
+	private function unparse_url( $parsed_url ) {
+		$scheme = isset( $parsed_url['scheme'] ) ? $parsed_url['scheme'] . '://' : '';
+		$host   = isset( $parsed_url['host'] ) ? $parsed_url['host'] : '';
+		if ( ! empty( $host ) && empty( $scheme ) ) {
+			$scheme = '//';
+		}
+		$port     = isset( $parsed_url['port'] ) ? ':' . $parsed_url['port'] : '';
+		$user     = isset( $parsed_url['user'] ) ? $parsed_url['user'] : '';
+		$pass     = isset( $parsed_url['pass'] ) ? ':' . $parsed_url['pass'] : '';
+		$pass     = ( $user || $pass ) ? "$pass@" : '';
+		$path     = isset( $parsed_url['path'] ) ? $parsed_url['path'] : '';
+		$query    = isset( $parsed_url['query'] ) ? '?' . $parsed_url['query'] : '';
+		$fragment = isset( $parsed_url['fragment'] ) ? '#' . $parsed_url['fragment'] : '';
+
+		return "$scheme$user$pass$host$port$path$query$fragment";
+	}
+
+	/**
 	 * Validate feeds attribute.
 	 *
 	 * @since   3.0.0
