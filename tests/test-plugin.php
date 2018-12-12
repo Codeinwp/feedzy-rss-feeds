71a2832fd2a92ddeeb3b3e10b8bc081da1b2915c
Ashish Ravi <contactashish13@gmail.com>
2018-12-12 16:13:06 +0530

    build failure

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -120,7 +120,9 @@ class Test_Feedzy extends WP_UnitTestCase {
 		$titles = array();
 		foreach ( $lists as $list ) {
 			if ( isset( $list['class'] ) && 'rss_item' === $list['class'] ) {
-				echo "list " . print_r($list,true);
+				// @codingStandardsIgnoreStart
+				echo "list " . print_r($list,true); 
+				// @codingStandardsIgnoreEnd
 				$titles[] = iconv( 'UTF-8', 'ASCII//IGNORE', trim( $list['span'][0]['a'][0]['#text'] ) );
 			}
 		}
