4c7db6a8a1fe5c5cb67862e4d6954d4bf1746b60
Ashish Ravi <contactashish13@gmail.com>
2019-12-30 11:41:42 +0530

    add links to documentation

--- includes/layouts/feedzy-tutorial.php
+++ includes/layouts/feedzy-tutorial.php
@@ -21,6 +21,19 @@ Layout For Tutorial Page of Feedzy RSS Feeds
 		</div>
 
 		<?php if ( class_exists( 'Feedzy_Rss_Feeds_Pro' ) ) { ?>
+		<div class="fz-feature">
+			<div class="fz-feature-features">
+				<h2>Shortcode templates to suit your style</h2>
+				<p><a href="https://docs.themeisle.com/article/1119-feedzy-rss-feeds-documentation#examples" target="_blank">Three beautiful templates</a> are available for you to choose the style which best fits your design. We even have support for complex media such as <a href="https://demo.themeisle.com/feedzy-rss-feeds/audio-feed-template/" target="_blank">audio playback</a> and <a href="https://docs.themeisle.com/article/1113-how-to-get-videos-from-youtube-with-feedzy" target="_blank">YouTube videos</a>.</p>
+				<p>If you like to roll your own templates, <a href="https://docs.themeisle.com/article/1162-feedzy-custom-templates" target="_blank">template tags</a> are available to help you do just that!</p>
+			</div>
+			<div class="fz-feature-image">
+				<img src="<?php echo FEEDZY_ABSURL; ?>/img/features-templates.jpg" alt="Feed templates">
+			</div>
+		</div>
+		<?php } ?>
+
+		<?php if ( class_exists( 'Feedzy_Rss_Feeds_Pro' ) ) { ?>
 		<a name="import"></a>
 		<div class="fz-feature">
 			<div class="fz-feature-features">
@@ -30,6 +43,18 @@ Layout For Tutorial Page of Feedzy RSS Feeds
 			</div>
 			<div class="fz-feature-image">
 				<iframe width="600" height="300" src="https://www.youtube.com/embed/Fzx5W_PfQsQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+		</div>
+		<?php } ?>
+
+		<?php if ( true === apply_filters( 'feedzy_is_license_of_type', false, 'agency' ) ) { ?>
+		<div class="fz-feature">
+			<div class="fz-feature-features">
+				<h2>Integration with SpinnerChief & WordAI</h2>
+				<p>Through WordAI integration, Feedzy will give you unlimited new content. Your SpinnerChief or WordAI subscriptions (not included) integrate seamlessly with Feedzy, so you won't ever have to worry about duplicate content - or Google penalties - again. <a href="https://docs.themeisle.com/article/746-how-to-use-wordai-to-rephrase-rss-content-in-feedzy" target="_blank">Check this out here.</a></p>
+			</div>
+			<div class="fz-feature-image">
+				<img src="<?php echo FEEDZY_ABSURL; ?>/img/feedzy-rss-feeds-wordai.jpg" alt="WordAi integration">
 			</div>
 		</div>
 		<?php } ?>
