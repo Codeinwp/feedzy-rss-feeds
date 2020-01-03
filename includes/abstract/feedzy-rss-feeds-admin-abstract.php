c107f59d26223598a95107844384d8caae2f13eb
Ashish Ravi <contactashish13@gmail.com>
2020-01-03 14:07:28 +0530

    backward compatibility

--- includes/abstract/feedzy-rss-feeds-admin-abstract.php
+++ includes/abstract/feedzy-rss-feeds-admin-abstract.php
@@ -547,6 +547,14 @@ abstract class Feedzy_Rss_Feeds_Admin_Abstract {
 		}
 
 		$feed = new Feedzy_Rss_Feeds_Util_SimplePie( $sc );
+		if ( ! $allow_https && method_exists( $feed, 'set_curl_options' ) ) {
+			$feed->set_curl_options(
+				array(
+					CURLOPT_SSL_VERIFYHOST => false,
+					CURLOPT_SSL_VERIFYPEER => false,
+				)
+			);
+		}
 		$feed->set_file_class( 'WP_SimplePie_File' );
 		$default_agent = $this->get_default_user_agent( $feed_url );
 		$feed->set_useragent( apply_filters( 'http_headers_useragent', $default_agent ) );
