626efdbdd70b0898135bcb881444589cfb35ef0f
Ashish Ravi <contactashish13@gmail.com>
2020-05-04 14:33:23 +0530

    gutenberg: manually delete the transient so that correct cache time can be used.

--- includes/gutenberg/feedzy-rss-feeds-gutenberg-block.php
+++ includes/gutenberg/feedzy-rss-feeds-gutenberg-block.php
@@ -198,6 +198,9 @@ class Feedzy_Rss_Feeds_Gutenberg_Block {
 			unset( $attr['sort'] );
 		}
 		$params = wp_parse_args( $attr );
+
+		// manually delete the transient so that correct cache time can be used.
+		delete_transient( 'feed_' . md5( $attr['feeds'] ) );
 		return feedzy_rss( $params );
 	}
 
