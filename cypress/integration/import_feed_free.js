f2dfc5c5a10cc0e5fe5124c8858a0bc546ff4ea5
Ashish Ravi <contactashish13@gmail.com>
2020-08-27 12:02:00 +0530

    cypress: convert type before comparing

--- cypress/integration/import_feed_free.js
+++ cypress/integration/import_feed_free.js
@@ -161,16 +161,16 @@ describe('Test Free - Import Feed', function() {
 
         // check last run status has all the data.
         cy.get('table.posts:nth-of-type(1) tr.feedzy-import-status-row td:nth-of-type(1) table tr:nth-of-type(1) td:nth-of-type(1)').invoke('data', 'value').should(($value) => {
-            expect(parseInt($value)).to.equal(feed.items); // found
+            expect(parseInt($value)).to.equal(parseInt(feed.items)); // found
         });
         cy.get('table.posts:nth-of-type(1) tr.feedzy-import-status-row td:nth-of-type(1) table tr:nth-of-type(1) td:nth-of-type(2)').invoke('data', 'value').should(($value) => {
             expect(parseInt($value)).to.equal(0); // duplicate
         });
         cy.get('table.posts:nth-of-type(1) tr.feedzy-import-status-row td:nth-of-type(1) table tr:nth-of-type(1) td:nth-of-type(3)').invoke('data', 'value').should(($value) => {
-            expect(parseInt($value)).to.equal(feed.items); // imported
+            expect(parseInt($value)).to.equal(parseInt(feed.items)); // imported
         });
         cy.get('table.posts:nth-of-type(1) tr.feedzy-import-status-row td:nth-of-type(1) table tr:nth-of-type(1) td:nth-of-type(4)').invoke('data', 'value').should(($value) => {
-            expect(parseInt($value)).to.equal(feed.items); // cumulative
+            expect(parseInt($value)).to.equal(parseInt(feed.items)); // cumulative
         });
         cy.get('table.posts:nth-of-type(1) tr.feedzy-import-status-row td:nth-of-type(1) table tr:nth-of-type(1) td:nth-of-type(5)').invoke('data', 'value').should(($value) => {
             expect(parseInt($value)).to.equal(1); // success
