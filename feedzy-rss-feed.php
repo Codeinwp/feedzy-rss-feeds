0658e4c5f2b52208a7daba7be0af117d4e5f57cf
selul <marius.cristea@vertistudio.com>
2020-12-09 12:17:25 +0200

    fix: remove 5.2 autoloading

--- feedzy-rss-feed.php
+++ feedzy-rss-feed.php
@@ -122,7 +122,7 @@ function run_feedzy_rss_feeds() {
 	define( 'FEEDZY_DISABLE_CACHE_FOR_TESTING', false );
 	$feedzy = Feedzy_Rss_Feeds::instance();
 	$feedzy->run();
-	$vendor_file = FEEDZY_ABSPATH . '/vendor/autoload_52.php';
+	$vendor_file = FEEDZY_ABSPATH . '/vendor/autoload.php';
 	if ( is_readable( $vendor_file ) ) {
 		require_once $vendor_file;
 	}
