e85d777a524c0d63d8dda7b022d1cdcde16481d9
selul <marius.cristea@vertistudio.com>
2021-04-07 17:02:09 +0300

    refactor docker install

---
+++ bin/cli-setup.sh
@@ -0,0 +1,23 @@
+
+# install WP
+wp  --allow-root  --quiet core install --url="http://localhost:8888/" --admin_user="wordpress" --admin_password="wordpress" --admin_email="test1@xx.com" --title="test" --skip-email
+
+# install required external plugins
+docker exec feedzy_wordpress chown -R www-data:www-data /var/www/html/
+docker exec feedzy_wordpress wp plugin install classic-editor --activate
+
+# activate
+wp  --allow-root  --quiet plugin activate feedzy-rss-feeds
+
+# set this constant so that the specific hooks are loaded
+wp  --allow-root  --quiet config set TI_CYPRESS_TESTING true --raw
+
+# create terms
+wp  --allow-root  --quiet term create category c_feedzy-1
+wp  --allow-root  --quiet term create post_tag t_feedzy-1
+
+# debugging
+wp  --allow-root  --quiet config set WP_DEBUG true --raw
+wp  --allow-root  --quiet config set WP_DEBUG_LOG true --raw
+wp  --allow-root  --quiet config set WP_DEBUG_DISPLAY false --raw
+
--- bin/wp-init.sh
+++ bin/wp-init.sh
@@ -1,36 +1,14 @@
 #!/usr/bin/env bash
 
-sleep_time=15
-echo "Sleeping for $sleep_time..."
-sleep $sleep_time
 
+export DOCKER_FILE=docker-compose.ci.yml
+# Wait for mysql container to be ready.
+while docker-compose -f $DOCKER_FILE run --rm -u root cli wp --allow-root db check ; [ $? -ne 0 ];  do
+	  echo "Waiting for db to be ready... "
+    sleep 1
+done
 # install WP
-docker exec  feedzy_wordpress wp --quiet core install --url="http://$wp_host:8888/" --admin_user="wordpress" --admin_password="wordpress" --admin_email="test1@xx.com" --title="test" --skip-email
-
-# install required external plugins
-docker exec feedzy_wordpress chown -R www-data:www-data /var/www/html/
-docker exec feedzy_wordpress wp plugin install classic-editor --activate
-
-# so that debug.log is created
-docker exec  feedzy_wordpress chmod 777 /var/www/html/wp-content
-
-# so that composer does not fail
-docker exec  feedzy_wordpress chmod -R 777 /var/www/html/wp-content/plugins/feedzy-rss-feeds-pro
-
-# activate
-docker exec  feedzy_wordpress wp --quiet plugin activate feedzy-rss-feeds
-
-# set this constant so that the specific hooks are loaded
-docker exec  feedzy_wordpress wp --quiet config set TI_CYPRESS_TESTING true --raw
-
-# create terms
-docker exec  feedzy_wordpress wp --quiet term create category c_feedzy-1
-docker exec  feedzy_wordpress wp --quiet term create post_tag t_feedzy-1
-
-# debugging
-docker exec  feedzy_wordpress wp --quiet config set WP_DEBUG true --raw
-docker exec  feedzy_wordpress wp --quiet config set WP_DEBUG_LOG true --raw
-docker exec  feedzy_wordpress wp --quiet config set WP_DEBUG_DISPLAY false --raw
+docker-compose -f $DOCKER_FILE run  --rm -u root cli bash -c "/var/www/html/bin/envs/cli-setup.sh"
 
 
 
