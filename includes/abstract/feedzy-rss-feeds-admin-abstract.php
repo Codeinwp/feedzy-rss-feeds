6d99c71b63c52e201daf8b361cf400b64463f9d4
girishkrishaweb <girish@krishaweb.com>
2021-04-30 15:09:08 +0530

    Improvement in DC namespace validation #541

--- includes/abstract/feedzy-rss-feeds-admin-abstract.php
+++ includes/abstract/feedzy-rss-feeds-admin-abstract.php
@@ -899,15 +899,11 @@ abstract class Feedzy_Rss_Feeds_Admin_Abstract {
 		if ( $feed->error() ) {
 			return false;
 		}
-
-		$feed_content = wp_remote_retrieve_body( wp_remote_get( $url ) );
-		if ( ! is_wp_error( $feed_content ) ) {
-			$xmlelement = new SimpleXMLElement( $feed_content );
-			$namespaces = $xmlelement->getNamespaces( true );
-			if ( array_key_exists( 'dc', $namespaces ) ) {
-				$feed_child = array_keys( $feed->get_item()->data['child'] );
-				$feed_child = array_filter( $feed_child );
-				if ( ! in_array( SIMPLEPIE_NAMESPACE_DC_10, $feed_child, true ) && ! in_array( SIMPLEPIE_NAMESPACE_DC_11, $feed_child, true ) ) {
+		// phpcs:ignore WordPress.Security.NonceVerification.NoNonceVerification
+		if ( isset( $_POST['feedzy_meta_data']['import_link_author_admin'] ) && 'yes' === $_POST['feedzy_meta_data']['import_link_author_admin'] ) {
+			if ( $feed->get_items() ) {
+				$author = $feed->get_items()[0]->get_author();
+				if ( empty( $author ) ) {
 					update_post_meta( $post->ID, '__transient_feedzy_invalid_dc_namespace', array( $url ) );
 					return false;
 				}
