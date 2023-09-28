8429972c3b2c70566307e756859ec470eab82ac4
girishpanchal30 <girish@krishaweb.com>
2023-09-28 14:27:50 +0530

    Fix undefined function error #798

--- cypress.config.js
+++ cypress.config.js
@@ -81,8 +81,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents( on, config ) {},
-    supportFile: false,
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
     baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     pageLoadTimeout : 300000,
