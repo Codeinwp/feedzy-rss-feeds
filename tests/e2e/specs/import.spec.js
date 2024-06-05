5e162f5bdb6a9f1e79f3ab74ac905232214d6af0
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-06-05 16:50:59 +0300

    chore: check the gallery

--- tests/e2e/specs/import.spec.js
+++ tests/e2e/specs/import.spec.js
@@ -208,14 +208,9 @@ test.describe( 'Feed Import', () => {
 
 		await runFeedImport( page );
 
-		// Select the first post created by feeds import. Check the featured image.
-		await page.getByRole('link', { name: 'Posts', exact: true }).click({ force: true });
-		await page.locator('#the-list tr').first().locator('a.row-title').click({ force: true });
-		await expect( page.getByLabel('Add title') ).toBeVisible(); // Post title.
-
-		if( ! await page.getByLabel('Edit or replace the image').isVisible() ) { // Should we open the featured image tab?
-			await page.getByRole('button', { name: 'Featured image' }).click({ force: true });
-		}
-		await expect( page.getByLabel('Edit or replace the image') ).toBeVisible(); // Featured image is added.
+		// Check the gallery.
+		await page.goto('/wp-admin/upload.php');
+		await page.waitForSelector('#wp-media-grid');
+		await expect( page.locator('.attachment').count() ).resolves.toBeGreaterThan(0); // We should have some imported images.
 	});
 });
