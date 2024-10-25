918ce0db9d3e24668171456331fc922f031d31f7
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-10-25 13:19:10 +0300

    fix: translate help us improve tab

--- includes/layouts/feedzy-improve.php
+++ includes/layouts/feedzy-improve.php
@@ -5,14 +5,17 @@
 			<img src="<?php echo esc_url( FEEDZY_ABSURL . 'img/improve-feedzy.png' ); ?>" alt="">
 		</div>
 		<div class="fz-left">
-			<h3 class="h3 pb-16">Answer a few questions to help us improve Feedzy</h3>
-			<p>We're always looking for suggestions to further improve Feedzy.
-			<?php if ( ! feedzy_is_pro() ) { ?>
-				 If your feedback is especially helpful and we choose to do an interview with you to discuss your suggestions, you will even gain a yearly membership for free for your trouble.
-			<?php } ?>
+			<h3 class="h3 pb-16"><?php esc_html_e( 'Answer a few questions to help us improve Feedzy', 'feedzy-rss-feeds' ); ?></h3>
+			<p>
+				<?php
+				esc_html_e( 'We\'re always looking for suggestions to further improve Feedzy.', 'feedzy-rss-feeds' );
+				if ( ! feedzy_is_pro() ) {
+					esc_html_e( 'If your feedback is especially helpful and we choose to do an interview with you to discuss your suggestions, you will even gain a yearly membership for free for your trouble.', 'feedzy-rss-feeds' );
+				}
+				?>
 			</p>
 			<div class="cta">
-				<a href="javascript:void(0)" id="fz-feedback-btn" class="btn btn-outline-primary" role="button">Take the Survey</a>
+				<a href="javascript:void(0)" id="fz-feedback-btn" class="btn btn-outline-primary" role="button"><?php esc_html_e( 'Take the Survey', 'feedzy-rss-feeds' ); ?></a>
 			</div>
 		</div>		
 	</div>
