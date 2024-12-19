1e6c0d1512aa800aeca085da2ebaf92b3e0a80bb
girishpanchal30 <girish@krishaweb.com>
2024-12-19 21:03:21 +0530

    Improve PHPUnit testcases

--- includes/admin/feedzy-rss-feeds-import.php
+++ includes/admin/feedzy-rss-feeds-import.php
@@ -1379,7 +1379,7 @@ class Feedzy_Rss_Feeds_Import {
 		$import_translation_lang  = get_post_meta( $job->ID, 'import_auto_translation_lang', true );
 		$import_post_author       = get_post_meta( $job->ID, 'import_post_author', true );
 		$mark_duplicate_tag       = get_post_meta( $job->ID, 'mark_duplicate_tag', true );
-		$mark_duplicate_tag       = ( defined( 'TI_UNIT_TESTING' ) || feedzy_is_pro() ) && ! empty( $mark_duplicate_tag ) ? preg_replace( '/[\[\]#]/', '', $mark_duplicate_tag ) : '';
+		$mark_duplicate_tag       = feedzy_is_pro() && ! empty( $mark_duplicate_tag ) ? preg_replace( '/[\[\]#]/', '', $mark_duplicate_tag ) : '';
 		$max                      = $import_feed_limit;
 
 		if ( metadata_exists( 'post', $job->ID, 'import_post_status' ) ) {
@@ -2092,7 +2092,7 @@ class Feedzy_Rss_Feeds_Import {
 			update_post_meta( $new_post_id, 'feedzy_item_author', sanitize_text_field( $author ) );
 
 			// Verify that the `$mark_duplicate_tag` does not match `'item_url'` to ensure the condition applies only when a different tag is specified.
-			if ( 'item_url' !== $mark_duplicate_tag ) {
+			if ( $mark_duplicate_tag && 'item_url' !== $mark_duplicate_tag ) {
 				update_post_meta( $new_post_id, 'feedzy_' . $mark_duplicate_tag, $duplicate_tag_value );
 			}
 
