892fa2a20809e9bdd91ac79850eabf6a919fb17c
girishpanchal30 <girish@krishaweb.com>
2024-12-19 11:36:46 +0530

    Add phpunit tests

--- includes/admin/feedzy-rss-feeds-import.php
+++ includes/admin/feedzy-rss-feeds-import.php
@@ -1379,7 +1379,7 @@ class Feedzy_Rss_Feeds_Import {
 		$import_translation_lang  = get_post_meta( $job->ID, 'import_auto_translation_lang', true );
 		$import_post_author       = get_post_meta( $job->ID, 'import_post_author', true );
 		$mark_duplicate_tag       = get_post_meta( $job->ID, 'mark_duplicate_tag', true );
-		$mark_duplicate_tag       = feedzy_is_pro() && ! empty( $mark_duplicate_tag ) ? preg_replace( '/[\[\]#]/', '', $mark_duplicate_tag ) : '';
+		$mark_duplicate_tag       = ( defined( 'TI_UNIT_TESTING' ) || feedzy_is_pro() ) && ! empty( $mark_duplicate_tag ) ? preg_replace( '/[\[\]#]/', '', $mark_duplicate_tag ) : '';
 		$max                      = $import_feed_limit;
 
 		if ( metadata_exists( 'post', $job->ID, 'import_post_status' ) ) {
@@ -1544,7 +1544,6 @@ class Feedzy_Rss_Feeds_Import {
 						$mark_duplicate_tag
 					);
 				}
-
 				if ( ! empty( $duplicate_tag_value ) ) {
 					$duplicate_tag_value = implode( ' ', $duplicate_tag_value );
 					$duplicate_tag_value = substr( sanitize_key( wp_strip_all_tags( $duplicate_tag_value ) ), 0, apply_filters( 'feedzy_mark_duplicate_content_limit', 256 ) );
