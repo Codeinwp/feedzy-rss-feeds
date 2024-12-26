6e2dfcc180051a1fb21fb64e0cb4d0f87aef017c
Hardeep Asrani <hardeepasrani@gmail.com>
2024-12-27 00:48:55 +0530

    feat: add filter conditions
    * [wip] Import Conditions
    
    * fix: e2e test failing
    
    * fix: unit tests
    
    * test: adding phpunit tests
    
    * fix: e2e test failing
    
    * fix: backward compat
    
    * Update ConditionsControl.js
    
    * chore: add dummy conditions for upsell
    
    * chore: allow editing of conditions without saving in free
    
    * feat: add filters support to shortcode
    
    * chore: improve upsell
    
    * fix: display of regex strings as special chars
    
    * chore: fix eslint error
    
    * chore: fix broken code
    
    * fix: the mess with escape and unescaping

---
+++ js/Conditions/index.js
@@ -0,0 +1,67 @@
+/**
+ * WordPress dependencies.
+ */
+import domReady from '@wordpress/dom-ready';
+
+import { createRoot, useEffect, useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies.
+ */
+import ConditionsControl from './ConditionsControl';
+
+const dummyConditions = {
+	match: 'all',
+	conditions: [
+		{
+			field: 'title',
+			operator: 'contains',
+			value: 'Sports',
+		},
+	],
+};
+
+const App = () => {
+	const [conditions, setConditions] = useState({
+		conditions: [],
+		match: 'all',
+	});
+
+	useEffect(() => {
+		if (!feedzyData.isPro) {
+			setConditions(dummyConditions);
+			return;
+		}
+
+		const field = document.getElementById('feed-post-filters-conditions');
+		if (field && field.value) {
+			const parsedConditions = JSON.parse(field.value);
+			setConditions(
+				parsedConditions && parsedConditions.conditions
+					? parsedConditions
+					: { conditions: [], match: 'all' }
+			);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (!feedzyData.isPro) {
+			return;
+		}
+
+		document.getElementById('feed-post-filters-conditions').value =
+			JSON.stringify(conditions);
+	}, [conditions]);
+
+	return (
+		<ConditionsControl
+			conditions={conditions}
+			setConditions={setConditions}
+		/>
+	);
+};
+
+domReady(() => {
+	const root = createRoot(document.getElementById('fz-conditions'));
+	root.render(<App />);
+});
