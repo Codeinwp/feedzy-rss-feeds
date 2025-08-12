ddc13880a278d5eb0a16e6ec9cd404eb3275b827
Soare Robert-Daniel <soare.robert.daniel@protonmail.com>
2025-08-12 15:47:49 +0300

    fix: better structure and module separation

--- js/ActionPopup/SortableItem.js
+++ js/ActionPopup/SortableItem.js
@@ -29,6 +29,8 @@ import {
 	SelectControl,
 } from '@wordpress/components';
 
+import RewriteActionItem from './RewriteActionItem';
+
 const DragHandle = sortableHandle(() => (
 	<Icon icon={dragHandle} size={18} className="components-panel__icon" />
 ));
@@ -89,34 +91,6 @@ const UpgradeNotice = ({ higherPlanNotice, utmCampaign }) => {
 	);
 };
 
-const aiModels = {
-	openai: {
-		latest: [
-			{ label: 'GPT-5 Nano', value: 'gpt-nano' },
-			{ label: 'GPT-5 Mini', value: 'gpt-5-mini' },
-			{ label: 'GPT-5', value: 'gpt-5' },
-			{ label: 'GPT-4.1 Nano', value: 'gpt-4.1-nano' },
-			{ label: 'GPT-4.1 Mini', value: 'gpt-4.1-mini' },
-			{ label: 'GPT-4.1', value: 'gpt-4.1' },
-			{ label: 'GPT-4o Mini', value: 'gpt-4o-mini' },
-			{ label: 'GPT-4o', value: 'gpt-4o' },
-			{ label: 'O4 Mini', value: 'o4-mini' },
-			{ label: 'O3 Mini', value: 'o3-mini' },
-			{ label: 'O1 Pro', value: 'o1-pro' },
-			{ label: 'O1', value: 'o1' },
-		],
-		older: [
-			{ label: 'GPT-4', value: 'gpt-4' },
-			{
-				label: 'GPT-3.5 Turbo Instruct',
-				value: 'gpt-3.5-turbo-instruct',
-			},
-			{ label: 'Babbage-002', value: 'babbage-002' },
-			{ label: 'Davinci-002', value: 'davinci-002' },
-		],
-	},
-};
-
 const CreditNotice = () => {
 	return (
 		<>
@@ -151,7 +125,6 @@ const getCurrentLanguage = () => {
 const SortableItem = ({ propRef, loopIndex, item }) => {
 	const counter = loopIndex + 1;
 
-	window.console.log(item.data);
 	if ('trim' === item.id) {
 		return (
 			<li className="fz-action-control" data-counter={counter}>
@@ -387,296 +360,13 @@ const SortableItem = ({ propRef, loopIndex, item }) => {
 	}
 
 	if ('chat_gpt_rewrite' === item.id) {
-		let defaultProvider = 'openai';
-		let providerLicenseStatus = false;
-		if (feedzyData.apiLicenseStatus.openaiStatus) {
-			defaultProvider = 'openai';
-		} else if (feedzyData.apiLicenseStatus.openRouterStatus) {
-			defaultProvider = 'openrouter';
-		}
-
-		const selectedProvider = item.data.aiProvider || defaultProvider;
-		if ('openai' === selectedProvider) {
-			providerLicenseStatus = feedzyData.apiLicenseStatus.openaiStatus;
-		} else if ('openrouter' === selectedProvider) {
-			providerLicenseStatus =
-				feedzyData.apiLicenseStatus.openRouterStatus;
-		}
-
-		let defaultModel = '';
-		if (selectedProvider === 'openai') {
-			defaultModel = feedzyData?.integrationSettings?.openai_api_model;
-		} else if (selectedProvider === 'openrouter') {
-			defaultModel =
-				feedzyData?.integrationSettings?.openrouter_api_model;
-		}
-
-		const selectedAIModel = item.data.aiModel || defaultModel;
-
 		return (
-			<li
-				className="fz-action-control fz-chat-cpt-action"
-				data-counter={counter}
-			>
-				<div className="fz-action-event">
-					{feedzyData.isPro &&
-						(feedzyData.isBusinessPlan ||
-							feedzyData.isAgencyPlan) &&
-						!providerLicenseStatus &&
-						(feedzyData.isHighPrivileges ? (
-							<span className="error-message">
-								{__('Invalid API Key', 'feedzy-rss-feeds')}{' '}
-								<ExternalLink
-									href={`admin.php?page=feedzy-integration&tab=${item.data.aiProvider || defaultProvider}`}
-								>
-									<Icon
-										icon={external}
-										size={16}
-										fill="#F00"
-									/>
-								</ExternalLink>
-							</span>
-						) : (
-							<span className="error-message">
-								{__(
-									'Invalid API Key, Please contact the administrator',
-									'feedzy-rss-feeds'
-								)}
-							</span>
-						))}
-					<PanelBody
-						title={__('Rewrite with AI', 'feedzy-rss-feeds')}
-						icon={DragHandle}
-						initialOpen={false}
-					>
-						<PanelRow>
-							<UpgradeNotice
-								higherPlanNotice={
-									!feedzyData.isBusinessPlan &&
-									!feedzyData.isAgencyPlan
-								}
-								utmCampaign="action-paraphrase-chatgpt"
-							/>
-							<BaseControl
-								__nextHasNoMarginBottom
-								className="mb-20"
-							>
-								<SelectControl
-									__nextHasNoMarginBottom
-									label={__(
-										'Choose an AI Provider',
-										'feedzy-rss-feeds'
-									)}
-									value={selectedProvider}
-									options={[
-										{
-											label: __(
-												'OpenAI',
-												'feedzy-rss-feeds'
-											),
-											value: 'openai',
-										},
-										{
-											label: __(
-												'OpenRouter',
-												'feedzy-rss-feeds'
-											),
-											value: 'openrouter',
-										},
-									]}
-									onChange={(currentValue) => {
-										propRef.onChangeHandler({
-											index: loopIndex,
-											aiProvider: currentValue ?? '',
-											aiModel: '',
-										});
-									}}
-									disabled={!feedzyData.isPro}
-								/>
-							</BaseControl>
-
-							{selectedProvider && aiModels[selectedProvider] && (
-								<BaseControl
-									__nextHasNoMarginBottom
-									className="mb-20"
-								>
-									<SelectControl
-										__nextHasNoMarginBottom
-										label={__(
-											'Choose Model',
-											'feedzy-rss-feeds'
-										)}
-										value={selectedAIModel}
-										onChange={(currentValue) => {
-											propRef.onChangeHandler({
-												index: loopIndex,
-												aiModel:
-													currentValue !==
-													defaultModel
-														? currentValue
-														: '',
-											});
-										}}
-										disabled={
-											!feedzyData.isPro ||
-											!providerLicenseStatus
-										}
-									>
-										{aiModels[selectedProvider].latest
-											.length > 0 && (
-											<optgroup
-												label={__(
-													'Latest models',
-													'feedzy-rss-feeds'
-												)}
-											>
-												{aiModels[
-													selectedProvider
-												].latest.map((model) => (
-													<option
-														key={model.value}
-														value={model.value}
-													>
-														{model.label}
-													</option>
-												))}
-											</optgroup>
-										)}
-
-										{aiModels[selectedProvider].older
-											.length > 0 && (
-											<optgroup
-												label={__(
-													'Older models',
-													'feedzy-rss-feeds'
-												)}
-											>
-												{aiModels[
-													selectedProvider
-												].older.map((model) => (
-													<option
-														key={model.value}
-														value={model.value}
-													>
-														{model.label}
-													</option>
-												))}
-											</optgroup>
-										)}
-									</SelectControl>
-								</BaseControl>
-							)}
-
-							<BaseControl __nextHasNoMarginBottom>
-								<TextareaControl
-									__nextHasNoMarginBottom
-									label={__(
-										'Main Prompt',
-										'feedzy-rss-feeds'
-									)}
-									help={sprintf(
-										// translators: %1$s is the tag content: {content}
-										__(
-											'You can use %1$s in the textarea such as: "Rephrase my %1$s for better SEO."',
-											'feedzy-rss-feeds'
-										),
-										'{content}'
-									)}
-									value={
-										item.data.ChatGPT
-											? unescape(
-													item.data.ChatGPT.replaceAll(
-														'&#039;',
-														"'"
-													)
-												)
-											: ''
-									}
-									onChange={(currentValue) =>
-										propRef.onChangeHandler({
-											index: loopIndex,
-											ChatGPT: currentValue ?? '',
-											aiProvider: selectedProvider,
-										})
-									}
-									disabled={
-										!feedzyData.isPro ||
-										!providerLicenseStatus
-									}
-								/>
-								<div className="fz-prompt-button">
-									<Button
-										variant="secondary"
-										onClick={() =>
-											propRef.updatePromptText({
-												index: loopIndex,
-												type: 'summarize',
-											})
-										}
-										disabled={
-											!feedzyData.isPro ||
-											!providerLicenseStatus
-										}
-									>
-										{__('Summarize', 'feedzy-rss-feeds')}
-									</Button>
-									<Button
-										variant="secondary"
-										onClick={() =>
-											propRef.updatePromptText({
-												index: loopIndex,
-												type: 'paraphase',
-											})
-										}
-										disabled={
-											!feedzyData.isPro ||
-											!providerLicenseStatus
-										}
-									>
-										{__('Paraphrase', 'feedzy-rss-feeds')}
-									</Button>
-									<Button
-										variant="secondary"
-										onClick={() =>
-											propRef.updatePromptText({
-												index: loopIndex,
-												type: 'change_tone',
-											})
-										}
-										disabled={
-											!feedzyData.isPro ||
-											!providerLicenseStatus
-										}
-									>
-										{__('Change tone', 'feedzy-rss-feeds')}
-									</Button>
-								</div>
-							</BaseControl>
-						</PanelRow>
-					</PanelBody>
-				</div>
-				<div className="fz-trash-action">
-					<button
-						type="button"
-						onClick={() => {
-							propRef.removeCallback(loopIndex);
-						}}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-						>
-							<path
-								d="M20 5.0002H14.3C14.3 3.7002 13.3 2.7002 12 2.7002C10.7 2.7002 9.7 3.7002 9.7 5.0002H4V7.0002H5.5V7.3002L7.2 18.4002C7.3 19.4002 8.2 20.1002 9.2 20.1002H14.9C15.9 20.1002 16.7 19.4002 16.9 18.4002L18.6 7.3002V7.0002H20V5.0002ZM16.8 7.0002L15.1 18.1002C15.1 18.2002 15 18.3002 14.8 18.3002H9.1C9 18.3002 8.8 18.2002 8.8 18.1002L7.2 7.0002H16.8Z"
-								fill="black"
-							/>
-						</svg>
-					</button>
-				</div>
-			</li>
+			<RewriteActionItem
+				counter={counter}
+				item={item}
+				loopIndex={loopIndex}
+				propRef={propRef}
+			/>
 		);
 	}
 
