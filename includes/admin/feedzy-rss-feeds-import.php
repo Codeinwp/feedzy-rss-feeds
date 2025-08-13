5519e63e978da605da016794b88fe9d21010ab9d
RaduCristianPopescu <119046336+RaduCristianPopescu@users.noreply.github.com>
2025-08-13 15:14:05 +0300

    fix: remove HTML tags after processing the item title (#1136)

--- includes/admin/feedzy-rss-feeds-import.php
+++ includes/admin/feedzy-rss-feeds-import.php
@@ -1879,6 +1879,10 @@ class Feedzy_Rss_Feeds_Import {
 				$post_title           = str_replace( '[#title_feedzy_rewrite]', $title_feedzy_rewrite, $post_title );
 			}
 
+			if ( is_string( $post_title ) ) {
+				$post_title = wp_strip_all_tags( $post_title );
+			}
+			
 			$image_html = '';
 			if ( ! empty( $item['item_img_path'] ) ) {
 				$image_html = '<img src="' . $item['item_img_path'] . '" title="' . $item['item_title'] . '" />';
