fdc42f67e20044dd2c140305839d60a2f7b29a16
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2025-08-14 09:55:21 +0300

    refactor: UI upsell modal for export/import, clone, import (#1138)

--- includes/views/js/import-metabox-edit.js
+++ includes/views/js/import-metabox-edit.js
@@ -1015,42 +1015,17 @@
 			'<div class="fz-header-action"></div>'
 		);
 
-		// Create dialog box
-		$('#fz_import_export_upsell').dialog({
-			title: '',
-			dialogClass: 'wp-dialog',
-			autoOpen: false,
-			draggable: false,
-			width: 'auto',
-			modal: true,
-			resizable: false,
-			closeOnEscape: true,
-			position: {
-				my: 'center',
-				at: 'center',
-				of: window,
-			},
-			open(event, ui) {
-				$('.ui-dialog-titlebar-close', ui.dialog | ui).hide();
-				$('.ui-dialog-titlebar', ui.dialog | ui).hide();
-			},
-			create() {
-				// style fix for WordPress admin
-				$('.ui-dialog-titlebar-close').addClass('ui-button');
-			},
-		});
-
 		$('.fz-export-import-btn.only-pro, .fz-export-btn-pro').on(
 			'click',
 			function (e) {
 				e.preventDefault();
-				$('#fz_import_export_upsell').dialog('open');
+				$('#fz_import_export_upsell').show();
 			}
 		);
 
 		$('#fz_import_export_upsell').on('click', '.close-modal', function (e) {
 			e.preventDefault();
-			$('#fz_import_export_upsell').dialog('close');
+			$('#fz_import_export_upsell').hide();
 		});
 
 		$(document).on(
