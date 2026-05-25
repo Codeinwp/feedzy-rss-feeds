5d5c5daa5958dc3893e47975bbc9539c1028b39a
girishpanchal30 <girish@krishaweb.com>
2026-05-25 14:39:02 +0530

    fix: phpcs

--- includes/admin/feedzy-rss-feeds-admin.php
+++ includes/admin/feedzy-rss-feeds-admin.php
@@ -1448,7 +1448,7 @@ class Feedzy_Rss_Feeds_Admin extends Feedzy_Rss_Feeds_Admin_Abstract {
 
 				$settings            = is_array( $settings ) ? $settings : array();
 				$settings['general'] = isset( $settings['general'] ) && is_array( $settings['general'] ) ? $settings['general'] : array();
- 				$settings['logs']    = isset( $settings['logs'] ) && is_array( $settings['logs'] ) ? $settings['logs'] : array();
+				$settings['logs']    = isset( $settings['logs'] ) && is_array( $settings['logs'] ) ? $settings['logs'] : array();
 
 				$settings['general']['disable-default-style'] = isset( $_POST['disable-default-style'] ) ? absint( wp_unslash( $_POST['disable-default-style'] ) ) : '';
 				$settings['general']['default-thumbnail-id']  = isset( $_POST['default-thumbnail-id'] ) ? absint( wp_unslash( $_POST['default-thumbnail-id'] ) ) : 0;
